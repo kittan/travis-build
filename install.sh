d2203409e55dae05093110ea149ce444767e1aa5
syama666 <yoshinorisugiyama@gmail.com>
2015-01-25 02:28:12 +0900

    Add travis

---
+++ install.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+install_libtool()
+{
+  sudo apt-get install libtool autoconf automake uuid-dev build-essential
+}
+
+
+install_zeromq()
+{
+  cd /tmp && mkdir -p zeromq
+  curl -L http://download.zeromq.org/zeromq-4.0.5.tar.gz | tar xvzf - -C zeromq --strip-components=1
+  cd zeromq
+  ./autogen.sh
+  ./configure --disable-dependency-tracking LDFLAGS="-lstdc++" --disable-shared --enable-static
+  make && sudo make install
+  rm -rf /tmp/zeromq
+  sudo ldconfig
+}
+
+install_libtool
+install_zeromq
