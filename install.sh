f2b6d4a1a48eb5a09bd647effeed7313e3e58d8d
syama666 <yoshinorisugiyama@gmail.com>
2015-01-25 02:38:54 +0900

    Fix: zeromq version up

--- install.sh
+++ install.sh
@@ -9,7 +9,7 @@ install_libtool()
 install_zeromq()
 {
   cd /tmp && mkdir -p zeromq
-  curl -L http://download.zeromq.org/zeromq-4.0.5.tar.gz | tar xvzf - -C zeromq --strip-components=1
+  curl -L http://download.zeromq.org/zeromq-4.1.0-rc1.tar.gz | tar xvzf - -C zeromq --strip-components=1
   cd zeromq
   ./autogen.sh
   ./configure --disable-dependency-tracking LDFLAGS="-lstdc++" --disable-shared --enable-static
